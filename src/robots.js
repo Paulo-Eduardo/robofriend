dd9b5305f4b6a4f9c79df71e2c582c2138c92344
Paulo-Eduardo <paulo.es@live.com>
2018-06-20 17:15:11 -0300

    first commit

---
+++ src/robots.js
@@ -0,0 +1,14 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Nayara Maggioni',
+        username: 'Nayara',
+        email: 'nayara@delicia.queromais'
+    },
+    {
+        id: 2,
+        name: 'Paulo Eduardo',
+        username: 'Paulo',
+        email: 'paulo@ama.anay'
+    }
+]
